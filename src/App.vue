a643a47d236a8754d99098b658f591ca4b6a808b
john3volta <workbox_1@tutanota.com>
2025-07-28 17:07:51 +0200

    Initial project setup: Vue 3 + TypeScript + SCSS structure

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from '@/components/layout/Header.vue'
+import Main from '@/components/layout/Main.vue'
+import Footer from '@/components/layout/Footer.vue'
+</script>
